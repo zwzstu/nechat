b4622498fedd3452cdd6d2292204262d5352472a
zwz <akazwzstu@outlook.com>
2023-04-09 00:20:33 +0800

    add edge config

--- app/api/keys/route.ts
+++ app/api/keys/route.ts
@@ -1,6 +1,9 @@
 import { get } from "@vercel/edge-config";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime: string = "edge";
+export const regions: string = "sfo1";
+
 export const GET =async (req: NextRequest) => {
     const keys = await get("keys") as string[];
     return NextResponse.json(keys);

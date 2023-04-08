95347e0374af9c7c371b10911e57c2351d3aa49b
zwz <akazwzstu@outlook.com>
2023-04-09 00:09:19 +0800

    test edge config

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,23 @@
-import { NextRequest } from "next/server";
+import { get } from '@vercel/edge-config';
+import { NextRequest, NextResponse } from "next/server";
 
-export const runtime: string = "experimental-edge";
+export const runtime: string = "edge";
 export const regions: string = "sfo1";
 
 export const GET = async (req: NextRequest) => {
-  const url: string = "http://ip.jsontest.com/";
-
-  const headers = new Headers();
-
-  return fetch(url, {
-    method: req.method,
-    headers,
-  });
+  const greeting = await get('greeting');
+  return NextResponse.json(greeting);
 };
 
 export const POST = async (req: NextRequest) => {
   const url: string = "https://api.openai.com/v1/chat/completions";
-
+  
   const headers = new Headers();
   headers.set("Authentication", `Bearer `);
 
   return fetch(url, {
     method: req.method,
     headers,
+    body: req.body,
   });
 };
